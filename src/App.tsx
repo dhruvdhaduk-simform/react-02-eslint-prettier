4b1e834ad2b978696b4fbea14100e5c2ed4541c8
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-08 11:49:54 +0530

    feature: Add name, email, github account in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 function App() {
-    return <div>Hello, World</div>;
+    return (
+        <div className="container">
+            <p>Hello, My name is Dhruv Dhaduk.</p>
+            <p>
+                My email is :{' '}
+                <a href="mailto:dhruv.dhaduk@simformsolutions.com">
+                    dhruv.dhaduk@simformsolutions.com
+                </a>
+            </p>
+            <p>
+                My GitHub account :{' '}
+                <a href="https://github.com/dhruvdhaduk-simform">
+                    https://github.com/dhruvdhaduk-simform
+                </a>
+            </p>
+        </div>
+    );
 }
 
 export default App;
